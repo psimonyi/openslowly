5cbbf645ba0b750d3a30f554b6586b2bd24cb5bd
Peter Simonyi <pts@petersimonyi.ca>
2018-07-15 21:38:22 -0400

    Add description for AMO

--- background.js
+++ background.js
@@ -2,13 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* Open Bookmarks Slowly: The default "Open All in Tabs" on bookmark folders
- * just opens bookmarks all at once; for large folders it warns about
- * performance but doesn't do anything differently.  This extension is a way to
- * open all the bookmarks a few at a time so they all have a chance to load
- * completely.
- */
-
 'use strict';
 
 const MENUID = 'menuitem';
